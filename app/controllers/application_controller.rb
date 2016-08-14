481f9c043744cf6554feaa33c5e02a4c270b3dc6
John Pistella <pistell2@illinois.edu>
2016-08-14 13:30:04 -0500

    Added the User Resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "hello, world"
+  	render html: "Hey Ma! It's Johnny!"
   end
 end
